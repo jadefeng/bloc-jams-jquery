447d051e2b3f18b5ea8e376346cff27b181f61d4
jadefeng <jade.jiefeng@gmail.com>
2019-03-21 22:18:00 -0700

    player bar button checkpoint

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -16,7 +16,9 @@
 
      song.element.on('click', event => {
        player.playPause(song);
+       $('button#play-pause').attr('playState', player.playState);
      });
+
 
     $('#song-list').append(song.element);    
 
